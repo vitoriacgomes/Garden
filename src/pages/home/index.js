a76269ca2f6c4376492d4b611839be55e0904709
vitoria <vi21cgomes@gmail.com>
2023-06-07 19:48:25 -0300

    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import inspo from "../../img/horta-na-cozinha.jpg"
 
 const Home = () => {
   return (
@@ -22,15 +23,15 @@ const Home = () => {
             <div class="container px-6 mx-auto">
                 <div class="flex flex-wrap -m-4">
                     <div class="xl:w-1/3 md:w-1/2 p-4">
-                        <div class="border border-gray-200 p-6 rounded-lg">
-                            <h2 class="text-lg text-400 font-medium  mb-2">Inspirações</h2>
+                        <div class="border p-6">
+                            <h2 class="text-lg text-400 font-medium  mb-2">  Inspirações</h2>
                             <p class="leading-relaxed text-base">Veja como preencher sua casa com as mais lindas mudas.</p>
-                        <a class="mt-3 text-500 inline-flex items-center">Learn More
+                        </div>
+                        <a class="mt-3 inline-flex items-center">Learn More
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
                             </svg>
                         </a>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -38,8 +39,8 @@ const Home = () => {
 
         <section class="text-gray-600 body-font">
             <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                    <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                <div class="lg:max-w-lg lg:w-full w-5/6 mb-10">
+                    <img class="object-cover object-center rounded" alt="hero" src={inspo} width={350}/>
                 </div>
                 <div class="texto-2 lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 class="title-font sm:text-5xl text-3xl mb-4 font-medium text-gray-900">Da semente
