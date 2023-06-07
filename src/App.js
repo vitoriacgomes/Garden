32a0eb71495e224c97d3b29f12c44aa8b7f65f93
vitoria <vi21cgomes@gmail.com>
2023-06-07 20:46:33 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Menu from './pages/menu';
 import Home from './pages/home';
+import Footer from './pages/Footer';
 
 function App() {
   return (
     <div className='container-app'>
       <Menu/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
